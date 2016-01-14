3c883f80b5c0f111fdfd0c6ab61a2ce408531a7d
Mike Brennan <mikiebrenbren@gmail.com>
2016-01-13 20:15:30 -0700

    commiting small change

--- src/main/java/com/mike/controller/Hello.java
+++ src/main/java/com/mike/controller/Hello.java
@@ -7,10 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,7 +22,7 @@ public class Hello {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping(Constants.HELLO+"/{name}")
+    @RequestMapping(value = Constants.HELLO+"/{name}" , method = RequestMethod.GET)
     public ResponseEntity<ResponseModel> helloWorld(HttpServletRequest request, HttpServletResponse response, @PathVariable final String name){
         logger.debug("This is the name: " + name);
         if(name == null){
